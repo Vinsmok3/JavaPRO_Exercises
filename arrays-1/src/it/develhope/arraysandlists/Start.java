ab6b65e8d13579de29f9c01ce0f42f1e3d069d39
Greed <cristiancoltea97@gmail.com>
2022-11-23 09:05:40 +0100

    Initial commit

---
+++ arrays-1/src/it/develhope/arraysandlists/Start.java
@@ -0,0 +1,19 @@
+package it.develhope.arraysandlists;
+
+
+
+
+    
+
+        // remember that array's index start from 0, not from 1!
+        
+
+        // here we use `new` to initialise an empty array of 6 items
+        
+
+        // populating the array
+        
+
+        // we use the method toString() from java.utils.Arrays
+        
+
---
+++ arrays-2/src/it/develhope/arraysandlists/Start.java
@@ -0,0 +1,15 @@
+package it.develhope.arraysandlists;
+
+
+
+        // here we compare if the arrays (the objects) are equal
+        
+
+        // here we compare the length
+ 
+
+        // first we sort the arrays, so that we can compare them later and understand if the arrays have the same tasks
+    
+
+        // now we can compare the sorted arrays
+      
